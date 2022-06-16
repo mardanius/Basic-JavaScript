84ef8dc336d81f19f350a47f56a9da31eb135546
mardanius <mariojeda.mg@gmail.com>
2022-06-16 07:41:34 -0400

    53 to 60

--- code.js
+++ code.js
@@ -993,3 +993,234 @@ function fun2() {
   }
   console.log(output);
 }
+
+fun2()
+
+
+
+
+/*===========================
+53. Local Scope and Functions
+=============================
+
+Variables which are declared within a function, as well as the function parameters, have local scope. That means they are 
+only visible within that function.
+
+Here is a function myTest with a local variable called loc.
+
+function myTest() {
+  const loc = "foo";
+  console.log(loc);
+}
+
+myTest();
+console.log(loc);
+The myTest() function call will display the string foo in the console. The console.log(loc) line (outside of the myTest 
+  function) will throw an error, as loc is not defined outside of the function.
+
+-----------------------------------------------------------------------------------------------------------------------
+The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. 
+Declare a local variable myVar inside myLocalScope and run the tests.
+
+Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail. */
+
+function myLocalScope() {
+  // Only change code below this line
+  let myVar = "10";
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+
+
+
+/*=====================================
+54. Global vs. Local Scope in Functions
+=======================================
+
+It is possible to have both local and global variables with the same name. When you do this, the local variable takes 
+precedence over the global variable.
+
+In this example:
+
+const someVar = "Hat";
+
+function myFun() {
+  const someVar = "Head";
+  return someVar;
+}
+The function myFun will return the string Head because the local version of the variable is present. */
+
+
+
+
+/*========================================================
+55. Understanding Undefined Value returned from a Function
+==========================================================
+
+A function can include the return statement but it does not have to. In the case that the function doesn't have a return 
+statement, when you call it, the function processes the inner code but the returned value is undefined.
+
+Example
+
+let sum = 0;
+
+function addSum(num) {
+  sum = sum + num;
+}
+
+addSum(3);
+addSum is a function without a return statement. The function will change the global sum variable but the returned value 
+of the function is undefined. */
+
+function sum(num) {
+  num += 3;
+  return num
+}
+
+console.log(sum(3))
+
+
+
+
+/*==================================
+56. Assignment with a Returned Value
+====================================
+
+If you'll recall from our discussion of Storing Values with the Assignment Operator, everything to the right of the equal 
+sign is resolved before the value is assigned. This means we can take the return value of a function and assign it to a 
+variable.
+
+Assume we have pre-defined a function sum which adds two numbers together, then:
+
+ourSum = sum(5, 12);
+will call the sum function, which returns a value of 17 and assigns it to the ourSum variable.
+
+------------------------------------------------------------------------------------------------------------
+Call the processArg function with an argument of 7 and assign its return value to the variable processed. */
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+
+
+
+/*===============
+57. Stand in Line
+=================
+
+In Computer Science a queue is an abstract Data Structure where items are kept in order. 
+New items can be added at the back of the queue and old items are taken off from the front of the queue.
+
+Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+
+Add the number to the end of the array, then remove the first element of the array.
+
+The nextInLine function should then return the element that was removed. */
+
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  let removed = arr.shift();
+  return removed;
+  // Only change code above this line
+}
+
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+
+
+
+/*==============================
+58. Understanding Boolean Values
+================================
+
+Another data type is the Boolean. Booleans may only be one of two values: true or false. They are basically little on-off 
+switches, where true is on and false is off. These two states are mutually exclusive.
+
+Note: Boolean values are never written with quotes. The strings "true" and "false" are not Boolean and have no special 
+meaning in JavaScript. */
+
+
+
+
+/*==========================================
+59. Use Conditional Logic with If Statements
+============================================
+
+if statements are used to make decisions in code. The keyword if tells JavaScript to execute the code in the curly braces 
+under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be 
+true or false.
+
+When the condition evaluates to true, the program executes the statement inside the curly braces. When the Boolean condition 
+evaluates to false, the statement inside the curly braces will not execute.
+
+Pseudocode 
+
+if (condition is true) {
+  statement is executed
+}
+Example */
+
+function test (myCondition) {
+  if (myCondition) {
+    return "It was true";
+  }
+  return "It was false";
+}
+
+console.log(test(true));
+console.log(test(false));
+
+/* test(true) returns the string It was true, and test(false) returns the string It was false.
+
+When test is called with a value of true, the if statement evaluates myCondition to see if it is true or not. 
+Since it is true, the function returns It was true. When we call test with a value of false, 
+myCondition is not true and the statement in the curly braces is not executed and the function returns It was false. */
+
+
+
+
+/*=======================================
+60. Comparison with the Equality Operator
+=========================================
+
+There are many comparison operators in JavaScript. All of these operators return a boolean true or false value.
+
+The most basic operator is the equality operator ==. The equality operator compares two values and returns true if they're 
+equivalent or false if they are not. Note that equality is different from assignment (=), which assigns the value on the 
+right of the operator to a variable on the left.
+
+function equalityTest(myVal) {
+  if (myVal == 10) {
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+If myVal is equal to 10, the equality operator returns true, so the code in the curly braces will execute, and the function 
+will return Equal. Otherwise, the function will return Not Equal. In order for JavaScript to compare two different data types 
+(for example, numbers and strings), it must convert one type to another. This is known as Type Coercion. 
+Once it does, however, it can compare terms as follows:
+
+1   ==  1  // true
+1   ==  2  // false
+1   == '1' // true
+"3" ==  3  // true */
