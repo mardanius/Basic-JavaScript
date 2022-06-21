7b28bbf82130f17dcb4f359f11d69dc37445acb1
mardanius <mariojeda.mg@gmail.com>
2022-06-21 08:00:50 -0400

    From 75 to 79

--- code.js
+++ code.js
@@ -1575,3 +1575,291 @@ function testSize(num) {
 }
 
 testSize(7);
+
+
+
+
+/*===========
+75. Golf Code
+=============
+
+In the game of Golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to 
+sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, 
+there is a different nickname.
+
+Your function will be passed par and strokes arguments. Return the correct string according to this table which lists 
+the strokes in order of priority; top (highest) to bottom (lowest):
+
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+par and strokes will always be numeric and positive. We have added an array of all the names for your convenience. */
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes === 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else if (strokes >= par + 3) {
+    return names[6];
+  }
+
+  //return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+
+
+
+/*====================================================
+76. Selecting from Many Options with Switch Statements
+======================================================
+
+If you have many options to choose from, use a switch statement. A switch statement tests a value and can have many case 
+statements which define various possible values. Statements are executed from the first matched case value until a break is 
+encountered.
+
+Here is an example of a switch statement:
+
+switch (lowercaseLetter) {
+  case "a":
+    console.log("A");
+    break;
+  case "b":
+    console.log("B");
+    break;
+}
+case values are tested with strict equality (===). The break tells JavaScript to stop executing statements. 
+If the break is omitted, the next statement will be executed.
+
+--------------------------------------------------------------------------------------
+Write a switch statement which tests val and sets answer for the following conditions:
+1 - alpha
+2 - beta
+3 - gamma
+4 - delta */
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+
+
+
+/*==============================================
+77. Adding a Default Option in Switch Statements
+================================================
+
+In a switch statement you may not be able to specify all possible values as case statements. 
+Instead, you can add the default statement which will be executed if no matching case statements are found. 
+Think of it like the final else statement in an if/else chain.
+
+A default statement should be the last case.
+
+switch (num) {
+  case value1:
+    statement1;
+    break;
+  case value2:
+    statement2;
+    break;
+...
+  default:
+    defaultStatement;
+    break;
+}
+
+--------------------------------------------------------------------
+Write a switch statement to set answer for the following conditions:
+a - apple
+b - bird
+c - cat
+default - stuff */
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+      break
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+
+
+
+/*=================================================
+78. Multiple Identical Options in Switch Statements
+===================================================
+
+If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. 
+If you have multiple inputs with the same output, you can represent them in a switch statement like this:
+
+let result = "";
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+    result = "1, 2, or 3";
+    break;
+  case 4:
+    result = "4 alone";
+}
+Cases for 1, 2, and 3 will all produce the same result.
+
+----------------------------------------------------------------
+Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+
+Note: You will need to have a case statement for each number in the range. */
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+
+
+
+/*======================================
+79. Replacing If Else Chains with Switch
+========================================
+
+If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+
+if (val === 1) {
+  answer = "a";
+} else if (val === 2) {
+  answer = "b";
+} else {
+  answer = "c";
+}
+can be replaced with:
+
+switch (val) {
+  case 1:
+    answer = "a";
+    break;
+  case 2:
+    answer = "b";
+    break;
+  default:
+    answer = "c";
+}
+
+-------------------------------------------------------------------
+Change the chained if/else if statements into a switch statement. */
+
+/*
+if (val === "bob") {
+  answer = "Marley";
+} else if (val === 42) {
+  answer = "The Answer";
+} else if (val === 1) {
+  answer = "There is no #1";
+} else if (val === 99) {
+  answer = "Missed me by this much!";
+} else if (val === 7) {
+  answer = "Ate Nine";
+} */
+
+// Only change code below this line
+switch (val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+}
+// Only change code above this line
+return answer;
+
+
+chainToSwitch(7);
